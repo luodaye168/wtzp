bf06d500b592213001411362244bc3fe543d9e9e
luodaye168 <1621710565@qq.com>
2022-04-03 20:14:59 +0800

    完成了主体结构

--- js/baseAPI.js
+++ js/baseAPI.js
@@ -8,6 +8,7 @@ $.ajaxPrefilter(function (options) {
             Authorization: localStorage.getItem('token') || ''
         }
     }
+    
 
     options.complete = function (res) {
         // if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败,token无效') {
